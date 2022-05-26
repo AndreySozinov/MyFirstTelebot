e660b92b182d5c0558cfdc7e611a4789c7892958
AndreySozinov <savrey@ya.ru>
2022-05-26 22:23:44 +0500

    telebot working

--- bot_commands.py
+++ bot_commands.py
@@ -3,19 +3,19 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import datetime
 from log import *
 
-async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def hi_command(update: Update, context: ContextTypes.context):
     logging(update, context)
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def help_command(update: Update, context: ContextTypes.context):
     logging(update, context)
     await update.message.reply_text(f'/hi\n/time\n/sum\n/help')
 
-async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def time_command(update: Update, context: ContextTypes.context):
     logging(update, context)
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
 
-async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def sum_command(update: Update, context: ContextTypes.context):
     logging(update, context)
     msg = update.message.text
     items = msg.split()
